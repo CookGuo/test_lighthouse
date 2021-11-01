fa5d641c73a105c73cad6f790d164fc0ff4890e3
郭民意 <moling@tsign.cn>
2021-11-01 10:47:59 +0800

    测试

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,12 @@ const b: number = a
 var c = b
 var s = ';;;'
 
-// s = 2
-
-var s = ';;;'
-
-var s = ';;;'
-
 const w: any = 3
 let cc = 'ss'
 
 const aaa = '--'
+
+const bbb = 'dgdg'
 
 // cc = 1
 
